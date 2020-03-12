ca10bed67edbd596c944464b6ac7bb5f3dceaf62
speier <kalman.speier@gmail.com>
2020-03-12 08:58:53 +0100

    data file location

--- pages/api/videos/[id].js
+++ pages/api/videos/[id].js
@@ -1,4 +1,4 @@
-import { videos } from '../data'
+import { videos } from './_data'
 
 export default ({ query: { id } }, res) => {
   const video = videos.find(v => v.id === id)
--- pages/api/videos/index.js
+++ pages/api/videos/index.js
@@ -1,4 +1,4 @@
-import { videos } from '../data'
+import { videos } from './_data'
 
 export default (req, res) => {
   res.status(200).json(videos)
