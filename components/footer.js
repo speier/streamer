8b522757347bf09d759e8c63081f69cc937b2aa6
speier <kalman.speier@gmail.com>
2020-03-18 09:45:36 +0100

    cleanup

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="p-4 ">
-      <div className="flex justify-center text-gray-400">© 2020 Kalman Speier</div>
-    </footer>
-  )
-}
-
-export default Footer
